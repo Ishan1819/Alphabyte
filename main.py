62b18fb62a3096ca32a796958d9e31ca78d130eb
Ishan Patil <ishanp141@gmail.com>
2025-03-04 09:19:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 from routes import router
-import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("upload.html", {"request": request})
-
+# Include the router
 app.include_router(router)
 
 if __name__ == "__main__":
